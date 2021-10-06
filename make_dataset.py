c22c2dba02c9a1f216f1aecaac3914fd59c27a5a
dalissonfigueiredo <dalissonfigueiredo@gmail.com>
2021-10-06 15:49:52 -0300

    removing intersection done

--- make_dataset.py
+++ make_dataset.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from typing import Union
 from tqdm import tqdm
 from scipy import signal
-
+from math import floor
 logging.basicConfig(filename='test.log', level=logging.DEBUG)
 
 
@@ -58,7 +58,7 @@ def wavwrite(file, samples, sr):
 
 
 class dataset():
-    def __init__(self, path, noise_path, sr, sec):
+    def __init__(self, path, noise_path, sr, sec, spk_percent):
         self.path = path
         self.noise_path = noise_path
         self.sr = sr
@@ -70,8 +70,14 @@ class dataset():
         self.total_len = len(self.len)
         self.noise_names = list_inputs(Path(self.noise_path), [])
         self.noise_len = len(self.noise_names)
+        assert min(self.len) > 4
+        self.spk_percent = spk_percent
 
     def fetch_signals(self, number_of_speakers):
+        '''
+        Busca os arquivos de audio e mistura sinais de ruido 
+        output : audio do tamanho especificado + ruidos
+        '''
         # read random speakers
         self.num_of_speakers = number_of_speakers
         # (num_speakers,singlas)
@@ -82,23 +88,22 @@ class dataset():
         
         #random input from speaker choice
         sig_ind_inp = [np.random.randint(0, high=self.len[i], size=1)[0] for i in sig_indx]
-        
+        masks = self.create_masks()
         names = list()
         for i in range(self.num_of_speakers):
             name = self.names[sig_indx[i]][sig_ind_inp[i]]
             logging.info(name.split('/')[-3])
             names.append(os.path.basename(name)[:-4])
-            s, fs = sf.read(name)
+            s = self.load_necessary_audio(sig_indx[i], sig_ind_inp[i])
             if len(s.shape) > 1:
                 s = s[:, 0]
             s = quantize(s)
-
             l = len(s)
             if l > self.size_of_signals:
                 noise_scale = np.std(s[0:self.size_of_signals]) / 80
                 temp_noise = np.random.randn(self.size_of_signals,) * noise_scale
                 signals[i] = temp_noise
-                signals[i, :] = signals[i, 0:l] + s[0:self.size_of_signals]
+                signals[i, :] = signals[i, 0:l] + (s[0:self.size_of_signals]*masks[i])
             else:
                 noise_scale = np.std(s) / 80
                 temp_noise = np.random.randn(self.size_of_signals,) * noise_scale
@@ -107,7 +112,18 @@ class dataset():
         logging.info('---')
         
         return signals, names
-
+    
+    def load_necessary_audio(self, speaker, input):
+        name = self.names[speaker][input]
+        s, _ = sf.read(name)
+        while(len(s) < self.size_of_signals):
+            n_input = np.random.randint(0, high=self.len[speaker])
+            if n_input == input:
+                continue
+            new_name = self.names[speaker][n_input]
+            n_s, _ = sf.read(new_name)
+            s = np.append(s, n_s)
+        return s
     def position_valid(self, room_dims, speaker_pos):
         if (speaker_pos[0] > 0.5 and speaker_pos[0] < room_dims[0]-0.5 and
                 speaker_pos[1] > 0.5 and speaker_pos[1] < room_dims[1]-0.5):
@@ -199,7 +215,11 @@ class dataset():
         #room dimensions
         H, H_anechoic = self.room_gen(secnario_room_dims, scenario_RT60)
 
+        
+        
         Mixed = np.zeros_like(S[0])
+        
+        
         conv_signals = np.zeros_like(S)
         angles_name = '_'
         for spk in range(scenario_num_of_speakers):
@@ -235,11 +255,26 @@ class dataset():
             s = s[0: self.sec * self.sr]
             s = s / 1.2 / np.max(np.abs(s))
             sf.write(name, s, self.sr)
+    
+    def create_masks(self):
+
+        empty_frames = int(floor(self.sr * (self.sec * self.spk_percent)))
+
+        #so vou permitir entre 1 e 3 alternacoes entre os speakers
+        mask = np.ones(self.size_of_signals)
+        start_point = int(floor((np.random.randint(80, 120)/100)*self.sr))
+        mask[start_point:(empty_frames+start_point)] = 0
+        opposing_mask = (mask == 0).astype(float)
+
+        return [mask, opposing_mask]
+
+
+
 
 
 def main(args):
   
-    Data = dataset(args.in_path, args.noise_path, args.sr, args.sec)
+    Data = dataset(args.in_path, args.noise_path, args.sr, args.sec, args.speaker_percent)
     if not os.path.exists(args.out_path):
         os.mkdir(args.out_path)
   
@@ -256,6 +291,7 @@ if __name__ == "__main__":
     parser.add_argument('--noise_path', type=str, default='/home/dalissonfigueiredo/repos/swave_facebook_implementation/dataset_creation/wham_noise/tt', help='')
     parser.add_argument('--num_of_speakers', type=int, default=2, help='no of speakers.')
     parser.add_argument('--num_of_scenes', type=int, default=10, help='no of examples.')
+    parser.add_argument('--speaker_percent', type=float, default=0.4, help='pencentage of audio to main speaker')
     parser.add_argument('--sec', type=int, default=4, help='')
     parser.add_argument('--sr', type=int, default=16000, help='')
     args = parser.parse_args()
